3a2253a1de08c5d12e9fdbd66dc7e842c23561ce
karamel <kenneth.odumah@gmail.com>
2022-06-12 13:43:09 +0400

    setup tddand pytest

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,11 +1,11 @@
-
 import os
 
 import pytest
 from starlette.testclient import TestClient
 
-from app.main import create_application  # updated
+from app.main import create_application
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -15,9 +15,42 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+
+        # testing
+        yield test_client
+
+    # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    # set up
     app = create_application()  # new
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:  # updated
+
+        # testing
+        yield test_client
+
+    # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
 
         # testing
         yield test_client
